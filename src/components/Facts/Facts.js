6e95ba56faea50396e1ec2da985d4522faa08a42
tikurila <karinapopova20044002pop@gmail.com>
2020-10-04 15:32:46 +0300

    facts

--- src/components/Facts/Facts.js
+++ src/components/Facts/Facts.js
@@ -1,50 +1,17 @@
-import React from 'react';
-import SlideFacts from "./SlideFacts.js";
-import SwiperCore, {Navigation, Pagination} from 'swiper';
-import {Swiper, SwiperSlide} from 'swiper/react';
-import 'swiper/swiper.scss';
-import 'swiper/components/navigation/navigation.scss';
-import 'swiper/components/pagination/pagination.scss';
-import './Facts.scss';
+import React from "react";
+import Slider from "../slider/slider";
 
-SwiperCore.use([Navigation, Pagination]);
+import "./Facts.scss";
 
 const Facts = ({title, slides}) => {
-	const factsSlides = slides.map((slideInfo) => <SwiperSlide key={slideInfo.id}>
-		<SlideFacts item={slideInfo}/>
-    </SwiperSlide>);
-    
-   return (
-    <section className="facts container">
-        <h2 className="facts-title">{title}</h2>
-        <div className="swiper-button-prev"/>
-  
-        <Swiper
-				navigation={{
-					nextEl: '.swiper-button-next',
-					prevEl: '.swiper-button-prev',
-				}}
-       pagination={{
-                el: '.facts-pagination',
-                clickable: true,
-                renderBullet: function (index, className) {
-                    return `<span class="facts-pagination-bullet ${className}"></span>`;
-                },
-            }}
-
-            spaceBetween={40}
-            slidesPerView={3}
-            centeredSlides={true}
-            loop={true}
-            loopFillGroupWithBlank={true}
-     > 
-     
-     {factsSlides}
-     </Swiper>
-     <div className="swiper-button-next"/>
-			<div className="facts-pagination"/>
-            </section>
-	);
+    return (
+        <section className="facts" id="facts">
+            <div className="container">
+                <h2 className="facts-title">{title}</h2>
+                <Slider slides={slides}/>
+            </div>
+        </section>
+    );
 };
 
 export default Facts;
